2b4025901b60fd7cf1a7da2ae64c45b824962f0d
João gabriel <joao.gabsilva1007@gmail.com>
2022-08-23 01:26:14 -0300

    refactor: component names

--- src/components/utils/typography/styles.ts
+++ src/components/utils/typography/styles.ts
@@ -1,19 +1,33 @@
 import { RFValue } from "react-native-responsive-fontsize";
-import styled from "styled-components/native";
+import styled, { DefaultTheme } from "styled-components/native";
 import { TextAlign, TypographyVariant } from "../..";
 
-const fontSizeDictionary = (theme) => ({
-  heading: theme.fontSize.md,
-  paragraph: theme.fontSize.sm,
-  text: theme.fontSize.sm,
-});
+const fontSizeDictionary = (
+  theme: DefaultTheme,
+  variant: TypographyVariant
+) => {
+  switch (variant) {
+    case "heading":
+      return theme.fontSize.md;
+    case "subtitle":
+      return theme.fontSize.sm;
+    case "paragraph":
+      return theme.fontSize.sm;
+    case "text":
+      return theme.fontSize.sm;
+  }
+};
 
 export const Text = styled.Text<{
   variant?: TypographyVariant;
   textAlign?: TextAlign;
+  bold?: boolean;
+  semibold?: boolean;
 }>`
   font-size: ${({ theme, variant }) =>
-    RFValue(fontSizeDictionary(theme)[variant])}px;
+    RFValue(fontSizeDictionary(theme, variant))}px;
   text-align: ${({ textAlign }) => textAlign};
   color: ${({ theme }) => theme.color.text.primary};
+  font-weight: ${({ bold, semibold }) =>
+    bold ? "bold" : semibold ? "700" : "normal"};
 `;
--- src/hooks/useTheme.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { themeContext } from "../themes/contexts/theme-context";
-
-export const useTheme = () => useContext(themeContext);
